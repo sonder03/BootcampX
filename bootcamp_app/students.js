d35ea7b143f0f99a274df39c3590a3070425e00c
sonder03 <ravneet.virk@gmail.com>
2023-07-22 13:23:00 -0400

    Save Parameterized Query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,16 +7,24 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
 
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-  })
-}).catch(err => console.error('query error', err.stack));
+
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+  pool.query(queryString, values).then(res => {
+    res.rows.forEach(row => {
+      console.log(`${row.student_id}: ${row.name}`);
+    })
+  });
+
+
